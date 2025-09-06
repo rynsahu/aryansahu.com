a02486d3eaa187d8f7c75f299048be3660fbf228
Aryan sahu <aryan.sahu@glance.com>
2025-09-06 17:57:42 +0530

    minor fix

--- components/header/index.js
+++ components/header/index.js
@@ -1,13 +1,11 @@
 export function navbar() {
-  return ``;
-
-  // return `
-  //   <nav class="nav-wrapper">
-  //     <ol>
-  //       <li><a href="/">Home</a></li>
-  //       <li><a href="/pointer-follower">Pointer Follower</a></li>
-  //       <li><a href="/pointer-follower">Pointer</a></li>
-  //     </ol>
-  //   </nav>
-  // `
+  return `
+    <nav class="nav-wrapper">
+      <ol>
+        <li><a href="/">Home</a></li>
+        <li><a href="/pointer-follower">Pointer Follower</a></li>
+        <li><a href="/pointer-follower">Pointer</a></li>
+      </ol>
+    </nav>
+  `
 }
