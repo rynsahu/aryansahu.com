1a2b436894618fa539a4182ab6dda5b7b29a3925
Aryan sahu <aryan.sahu@glance.com>
2025-09-06 17:46:58 +0530

    Added basic app structure

---
+++ components/header/index.js
@@ -0,0 +1,13 @@
+export function navbar() {
+  return ``;
+
+  // return `
+  //   <nav class="nav-wrapper">
+  //     <ol>
+  //       <li><a href="/">Home</a></li>
+  //       <li><a href="/pointer-follower">Pointer Follower</a></li>
+  //       <li><a href="/pointer-follower">Pointer</a></li>
+  //     </ol>
+  //   </nav>
+  // `
+}
---
+++ pointer-follower/index.js
@@ -0,0 +1,5 @@
+import { render } from '../lib/flow/index.js';
+import { navbar } from '../components/header/index.js';
+
+const navbarEl = document.getElementById('navbar');
+render(navbar(), navbarEl);
