a675e19a33f17b28f8d15183541603971a3a903f
Aryan sahu <aryan.sahu@glance.com>
2025-09-11 19:29:14 +0530

    Added slide v2

--- lerp/progressbar/progressbar.js
+++ lerp/progressbar/progressbar.js
@@ -1,44 +1,21 @@
 export function progressbar() {
-  const currentProgressState = document.querySelector('.progress-state');
-  const grabber = document.querySelector('.progress-state .grabber');
+  const sliderInput = document.querySelector('#slider-input');
+  const quantityEl = document.querySelector('.quantity span');
+  const totalPriceEl = document.querySelector('.total .price');
+  const diamondPrice = 8455.00;
 
-  let currentGrabberPosition = 0;
-  let currentState = 0;
-
-  function handleMouseMove({ clientX, changedTouches }) {
-    let currentPointerPosition = clientX;
-    
-    if (changedTouches && changedTouches.length) x = changedTouches[0].clientX;
-
-    currentState = currentPointerPosition > currentGrabberPosition ? currentState + 1 : currentState - 1;
-    
-    if (currentState < 0) currentState = 0;
-    else if (currentState > 100) currentState = 100;
-
-    currentProgressState.style.setProperty('--state', currentState);
+  function updateQuantity(quantity) {
+    quantityEl.textContent = quantity;
   }
 
-
-  function handleMouseUp() {
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseup', handleMouseUp);
-    document.removeEventListener('touchmove', handleMouseMove);
-    document.removeEventListener('touchend', handleMouseUp);
-
-    console.log('UP')
+  function updateTotalPrice(quantity) {
+    totalPriceEl.textContent = `₹ ${diamondPrice * quantity}.00`;
   }
 
-  function handleMouseDown(e) {
-    currentGrabberPosition = e.clientX;
+  sliderInput.addEventListener('input', (e) => {
+    const quantity = parseInt(e.target.value);
 
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-    document.addEventListener('touchmove', handleMouseMove);
-    document.addEventListener('touchend', handleMouseUp);
-
-    console.log('DOWN')
-  }
-
-  grabber.addEventListener('mousedown', handleMouseDown);
-  grabber.addEventListener('touchstart', handleMouseDown);
+    updateQuantity(quantity);
+    updateTotalPrice(quantity);
+  });
 }
