c15b013dbf1c71d69690c6915bfaf32062f9439d
Aryan sahu <rynsahu@gmail.com>
2026-04-27 01:15:37 +0530

    vercel build fix

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { readFileSync } from "node:fs";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const isProduction = process.env.NODE_ENV === 'production';
+
 async function createServer() {
     const app = express();
-    const PORT = process.env.PORT || 5173;
-
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const isProduction = process.env.NODE_ENV === 'production'
 
     let vite;
 
@@ -19,14 +18,14 @@ async function createServer() {
             appType: "custom",
             server: { middlewareMode: true },
         });
-    
+
         app.use(vite.middlewares);
     } else {
         const compression = (await import('compression')).default;
         const serveStatic = (await import('serve-static')).default;
 
         app.use(compression());
-        app.use('/assets', serveStatic(path.join(__dirname, 'dist/client/assets')))
+        app.use('/assets', serveStatic(path.join(__dirname, 'dist/client/assets')));
     }
 
     app.use('*all', async (req, res, next) => {
@@ -37,7 +36,6 @@ async function createServer() {
         if (!isProduction) {
             template = readFileSync(path.join(__dirname, 'index.html'), 'utf8');
             template = await vite.transformIndexHtml(url, template);
-    
             render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
         } else {
             template = readFileSync(path.join(__dirname, 'dist/client/index.html'), 'utf-8');
@@ -45,15 +43,21 @@ async function createServer() {
         }
 
         const { html: appHtml } = render(url);
-
         const html = template.replace('<!--ROOT_OUTLET-->', appHtml);
 
         res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
     });
 
+    return app;
+}
+
+const app = await createServer();
+
+if (!isProduction) {
+    const PORT = process.env.PORT || 5173;
     app.listen(PORT, () => {
-        console.info(`Server is listing on ${PORT}...`);
+        console.info(`Server is listening on ${PORT}...`);
     });
 }
 
-createServer();
+export default app;
